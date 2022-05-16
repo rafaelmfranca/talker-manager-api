7e03ab286aba863c6140e43c0f42c1f00df20aac
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-16 15:09:05 -0300

    ✨ (routes): Add GET for '/talker/:id'

--- routes/talker.js
+++ routes/talker.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const STATUS_CODE = require('../constants/httpStatus');
+const MESSAGE = require('../constants/messages');
 const { getTalkers } = require('../helpers/fs');
 
 const router = express.Router();
@@ -9,6 +10,23 @@ router.route('/').get(
   rescue(async (_req, res) => {
     const talkers = await getTalkers();
     res.status(STATUS_CODE.OK).json(talkers);
+  }),
+);
+
+router.route('/:id').get(
+  rescue(async (req, res) => {
+    const { id } = req.params;
+
+    const talkers = await getTalkers();
+    const specifiedTalker = talkers.find((talker) => talker.id === Number(id));
+
+    if (!specifiedTalker) {
+      res
+        .status(STATUS_CODE.NOT_FOUND)
+        .json({ message: MESSAGE.TALKER_NOT_FOUND });
+    }
+
+    res.status(STATUS_CODE.OK).json(specifiedTalker);
   }),
 );
 
